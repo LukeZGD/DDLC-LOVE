271ee70902f9d8ef07cdb313e7f37bcc4959d7eb
LukeeGD <l32luke@gmail.com>
2018-06-03 10:06:59 +0800

    Add files via upload

--- game/states/game.lua
+++ game/states/game.lua
@@ -7,7 +7,6 @@ function drawGame()
 	love.graphics.setColor(255, 255, 255, alpha)
 	love.graphics.draw(bgch, 0, 0)
 	if xaload >= 1 and menu_enabled == false or menu_type == 'choice' then
-		
 		if cg1 ~= "" then love.graphics.draw(cgch, 0, 0) end
 		drawSayori(s.a,s.b)
 		drawYuri(y.a,y.b) 
@@ -19,7 +18,7 @@ function drawGame()
 		if ct ~= '' then love.graphics.draw(namebox, 52, 142) end
 		love.graphics.draw(textbox, 40, 162)
 		love.graphics.setColor(0,0,0)
-		if ct then love.graphics.print(ct,20,142) end
+		if ct then love.graphics.print(ct,60,142) end
 		if ca then love.graphics.print(ca,48,166) end
 		if cb then love.graphics.print(cb,48,182) end
 		if cc then love.graphics.print(cc,48,198) end
@@ -58,7 +57,7 @@ function drawGame()
 		love.graphics.print("Y - Pause",40,c_x,0,1,1)
 		love.graphics.print("B - Auto",133,c_x,0,1,1)
 		love.graphics.print("X - Skip",225,c_x,0,1,1)
-		--love.graphics.print(xaload,0,220) 
+		love.graphics.print(cl,0,220) 
 	end
 	if menu_enabled then menu_draw() end
 end
