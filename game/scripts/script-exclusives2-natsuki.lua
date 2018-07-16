a98ab0dad30307bf3589fd4476c2f07803942b35
LukeeGD <l32luke@gmail.com>
2018-07-16 10:10:10 +0800

    Fix errors
    - And make one of Natsuki's dialog fully readable on 3DS
    
    
    Former-commit-id: 036434226a48391b621b27b54c34ce48dc0afae2

--- game/scripts/script-exclusives2-natsuki.lua
+++ game/scripts/script-exclusives2-natsuki.lua
@@ -265,7 +265,8 @@ function Natsuki_exclusive2_1()
     n "I mean, I feel like I can't even keep it in my own room..."
 	elseif cl == 470 then
 	style_edited = true
-    n "My dad would beat the shit out of me if he found this."
+	ca = '"My dad would beat the shit out of me'
+	cb = 'if he found this."'
 	elseif cl == 471 then
     style_edited = false
     n "At least it's safe here in the clubroom."
