352a70c6bb92696a814735b10d067c7f0e942fa2
LukeeGD <l32luke@gmail.com>
2018-08-07 21:39:38 +0800

    Act 3 end!
    and a small bit of act 4 script
    
    
    Former-commit-id: 90975d03516a8bb36168f6cedff15cef17dfbde5
    Former-commit-id: 3e52d67846bc4533453a5f32266ea166da2d7b76

---
+++ game/scripts/script-ch40.lua
@@ -0,0 +1,22 @@
+function ch40script()
+	if cl == 1 then
+	bgUpdate('residential')
+	audioUpdate('2')
+    bl "It's an ordinary school day, like any other."
+	elseif cl == 2 then
+    bl "As usual, I'm surrounded by couples and friend groups walking to school together."
+	elseif cl == 3 then
+    bl "I always tell myself it's about time I meet some girls or something like that..."
+	elseif cl == 4 then
+	updateSayori('1','a',80)
+    cw('s',"Hey, "..player.."...")
+	elseif cl == 5 then
+    bl "...Well, there already is one girl."
+	elseif cl == 6 then
+    bl "That girl is Sayori, my neighbor and good friend since we were children."
+	elseif cl == 7 then
+    bl "We used to walk to school together every day..."
+	elseif cl == 8 then
+    bl "...And recently, we've picked up that habit once again."
+    end
+end
