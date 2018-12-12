20b9a06540332e54f32d8981e727d2f5c3e5d594
LukeeGD <l32luke@gmail.com>
2018-12-12 10:29:41 +0800

    poem game: words should no longer duplicate
    Former-commit-id: 9c3b83c32b11e39900a04818bda2cbea33d5e29c

--- game/draw.lua
+++ game/draw.lua
@@ -31,7 +31,7 @@ end
 
 function outlineText(text,x,y,type)
 	lg.setColor(0,0,0,alpha)
-	if not lv1lua.isPSP then
+	if g_system ~= 'PSP' then
 		lg.print(text,x-1.25,y)
 		lg.print(text,x,y-1.25)
 		lg.print(text,x+1.25,y)
