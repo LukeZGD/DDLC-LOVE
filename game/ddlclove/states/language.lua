0e5df3315ba4b6fe87bcc136020650b92b06706f
Darkmet98 <darkmet98@gmail.com>
2020-03-25 17:33:47 +0100

    Deleted ITA language (I have this only for testing purposes)

--- game/ddlclove/states/language.lua
+++ game/ddlclove/states/language.lua
@@ -1,5 +1,5 @@
-lang_names = {'English', 'Español', 'Italiano'}
-lang_codes = {'eng', 'esp', 'ita'}
+lang_names = {'English', 'Español castellano'}
+lang_codes = {'eng', 'esp'}
 local lang_state = 0
 local lang_select = 1
 
